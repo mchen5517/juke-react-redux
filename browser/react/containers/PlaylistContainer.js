39ce3ffb704a330f68f8e66bb8d0efae9874bd3b
Michael Chen <michael.chen.cse@gmail.com>
2017-04-03 16:01:24 -0400

    whoops

--- browser/react/containers/PlaylistContainer.js
+++ browser/react/containers/PlaylistContainer.js
@@ -2,38 +2,53 @@ import React, {Component} from 'react';
 import store from '../store';
 import Playlist from '../components/Playlist';
 import {toggleSong} from '../action-creators/player';
+import {connect} from 'react-redux';
 
-class PlaylistContainer extends Component {
+// class PlaylistContainer extends Component {
 
-  constructor() {
-    super();
-    this.state = store.getState();
+//   constructor() {
+//     super();
+//     this.state = store.getState();
+//   }
+
+//   componentDidMount() {
+//     this.unsubscribe = store.subscribe(() => {
+//       this.setState(store.getState());
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribe();
+//   }
+
+//   toggle(song, list) {
+//     store.dispatch(toggleSong(song, list));
+//   }
+
+//   render() {
+//     return (
+//       <Playlist
+//         {...this.state.player}
+//         selectedPlaylist={this.state.playlists.selected}
+//         toggleOne={this.toggle}
+//       />
+//     );
+//   }
+
+// }
+
+// export default PlaylistContainer;
+
+export default connect(state => {
+  return {
+    selectedPlaylist={state.playlists.selected}
+    currentSong: state.player.currentSong,
+    isPlaying: state.player.isPlaying
   }
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState());
-    });
+},
+(dispatch) => {
+  return {
+    toggleOne: (song, list) => dispatch(toggleSong(song, list))
   }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  toggle(song, list) {
-    store.dispatch(toggleSong(song, list));
-  }
-
-  render() {
-    return (
-      <Playlist
-        {...this.state.player}
-        selectedPlaylist={this.state.playlists.selected}
-        toggleOne={this.toggle}
-      />
-    );
-  }
-
 }
-
-export default PlaylistContainer;
+)(Playlist);
