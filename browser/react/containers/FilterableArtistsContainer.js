bf7de0250f14c108772c1c421981b6909478a541
Michael Chen <michael.chen.cse@gmail.com>
2017-04-03 17:51:13 -0400

    donezo with non-bono

--- browser/react/containers/FilterableArtistsContainer.js
+++ browser/react/containers/FilterableArtistsContainer.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import FilterInput from '../components/FilterInput';
 import Artists from '../components/Artists';
+import {connect} from 'react-redux';
 
 import store from '../store';
 
 class FilterableArtistsContainer extends React.Component {
 
-  constructor() {
+  constructor(props) {
 
-    super();
+    super(props);
 
-    this.state = Object.assign({
+    // this.state = Object.assign({
+    //   inputValue: ''
+    // }, props.artists);
+
+    this.state = {
       inputValue: ''
-    }, store.getState().artists);
+    }
 
     this.handleChange = this.handleChange.bind(this);
 
-  }
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState().artists);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
   }
 
   handleChange(evt) {
@@ -37,7 +32,7 @@ class FilterableArtistsContainer extends React.Component {
   render() {
 
     const inputValue = this.state.inputValue;
-    const filteredArtists = this.state.list.filter(artist => artist.name.match(inputValue));
+    const filteredArtists = this.props.list.filter(artist => artist.name.match(inputValue));
 
     return (
       <div>
@@ -51,17 +46,13 @@ class FilterableArtistsContainer extends React.Component {
   }
 }
 
-export default FilterableArtistsContainer;
+// export default FilterableArtistsContainer;
 
-// export default connect(
-//   )(() => {
-//     return (
-//       <div>
-//         <FilterInput
-//           handleChange={this.handleChange}
-//           inputValue={inputValue}
-//         />
-//         <Artists artists={filteredArtists}/>
-//       </div>
-//     );
-// })
+
+export default connect(
+    state => {
+      return {
+        list: state.artists.list
+      }
+    }
+  )(FilterableArtistsContainer);
